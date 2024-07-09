64653f73b31814ce14b400758e5a3256d7b31d97
hieunq <hieunq@vtc.vn>
2024-07-09 18:52:03 +0700

    clean code

--- src/app/_trpc/serverClient.ts
+++ src/app/_trpc/serverClient.ts
@@ -1,13 +1,5 @@
-import { httpBatchLink } from '@trpc/client';
 import { appRouter } from '@/server';
 import { server } from '@/server/trpc';
 
-// export const serverClient = appRouter.createCaller({
-//      links:[
-//         httpBatchLink({
-//             url: process.env.BASE_API || ""
-//         })
-//      ]
-// })
 const createCaller = server.createCallerFactory(appRouter);
 export const serverClient = createCaller({});
